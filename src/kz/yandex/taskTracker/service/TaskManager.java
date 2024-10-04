74f06cadba31cada2042a2c9c88122db314024d6
Khs <saha20142@yandex.kz>
2024-10-04 13:48:44 +0500

    Исправил ошибки при проверке на CodeStyle

--- src/kz/yandex/taskTracker/service/TaskManager.java
+++ src/kz/yandex/taskTracker/service/TaskManager.java
@@ -10,26 +10,42 @@ import java.util.List;
 
 public interface TaskManager {
     void addTask(Task task);
+
     Task getTask(int id);
+
     void updateTask(Task task);
+
     void removeTask(int id);
+
     void removeAllTasks();
 
     void addSubtask(Subtask subtask);
+
     Subtask getSubtask(int id);
+
     void updateSubtask(Subtask subtask);
+
     void removeSubtask(int id);
+
     void removeAllSubtasks();
 
     void addEpic(Epic epic);
+
     Epic getEpic(int id);
+
     void updateEpic(Epic epic);
+
     void removeEpic(int id);
+
     void removeAllEpics();
 
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
+
     List<Subtask> getEpicSubtasks(int epicId);
+
     List<Task> getHistory();
 }
