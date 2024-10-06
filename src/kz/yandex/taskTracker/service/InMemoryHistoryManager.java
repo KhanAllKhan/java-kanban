a8b18c3f310d37396e29eced0347ed9aebaa9de8
Khs <saha20142@yandex.kz>
2024-10-06 13:43:47 +0500

    попытка сдать работу

--- src/kz/yandex/taskTracker/service/InMemoryHistoryManager.java
+++ src/kz/yandex/taskTracker/service/InMemoryHistoryManager.java
@@ -10,15 +10,13 @@ public class InMemoryHistoryManager implements HistoryManager {
     private final int MAX_SIZE_HISTORY = 10;
 
     @Override
-    public void remove(int id) {
-
-    }
+    public void remove(int id) {}
 
     @Override
     public void add(Task task) {
         if (history.size() < MAX_SIZE_HISTORY) {
             history.add(task);
-        }else {
+        } else {
             history.remove(0);// Удаляем самый старый элемент
         }
 
